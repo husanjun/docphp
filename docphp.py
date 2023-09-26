827a1243ef82c6b4847119c8d765c8a4c1db6b37
hujunjun <1466472672@qq.com>
2023-09-26 16:16:54 +0800

    fix bug

--- docphp.py
+++ docphp.py
@@ -422,10 +422,10 @@ class DocphpShowDefinitionCommand(sublime_plugin.TextCommand):
     def handle_code_block(self, obj):
         content = re.sub(r'<br\s*?/?>', '\n', obj.group(1))
         content = re.sub(r'</?\w+[^>]*>', '', content, flags=re.S)
-        # return '\n\n```php{}\n```\n'.format(HTMLParser().unescape(content))
-        print(content)
         content = content.replace('```php', '')
-        return '\n\n```php\n{}\n```\n'.format(content)
+        return '\n\n```php{}\n```\n'.format(HTMLParser().unescape(content))
+        # print(content)
+        # return '\n\n```php\n{}\n```\n'.format(content)
 
     def formatPopup(self, content, symbol, can_back=False):
         if not isinstance(content, str):
